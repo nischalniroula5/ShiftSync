898c966cf0f715a59ba00751fdc69dcca073e80f
Nischal Niroula <officialnischalniroula@gmail.com>
2023-12-24 12:30:01 +1100

    Initial Commit

---
+++ ShiftSync/ShiftSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftSyncApp.swift
+//  ShiftSync
+//
+//  Created by Nischal Niroula on 24/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShiftSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
